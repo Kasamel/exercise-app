0b41ecc3ffa6c464a566f711354a871449dff8c1
Михаил <bulanev2012@yandex.ru>
2020-07-21 22:45:39 +0300

    add functinality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeItem } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeBook } from './recipe-book/recipe-book.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingList } from './shopping-list/shopping-list.component';
+import { RecipeDetail } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { ShoppingListEdit } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeItem,
+    RecipeBook,
+    HeaderComponent,
+    ShoppingList,
+    RecipeDetail,
+    RecipeListComponent,
+    ShoppingListEdit
   ],
   imports: [
     BrowserModule
